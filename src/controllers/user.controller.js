a3eb140f9b9fcd99c47c84216e48dc7510037ce3
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-06-25 21:47:31 -0700

    register user logic & algorithm step by step

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,17 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-   res.status(201).json({
-      message: "OK",
-   });
+   /**
+    * Get user details from frontend
+    * validation data -> not empty
+    * check if user already exist or not : username or email
+    * Check for images, check for avatar and coverImage
+    * upload them to cloudinary, avatar
+    * create a user object-> create a new entry to db
+    * Remove password and refresh token from response
+    * Check for user creation
+    * return response
+    */
 });
 
 export { registerUser };
