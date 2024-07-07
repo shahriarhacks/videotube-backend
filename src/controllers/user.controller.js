0e33230e818d635ce8c8f52e636f7f91351b2c41
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-07-07 14:14:50 +0600

    add getUserChannelProfile with aggregate

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -365,6 +365,77 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
       .json(new APIResponse(200, user, "User Avatar Updated Done!"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+   const { username } = req.params;
+
+   if (!username?.trim()) {
+      throw new APIError(400, "username is missing!!");
+   }
+
+   const channel = await User.aggregate([
+      {
+         $match: { username: username?.toLowerCase() },
+      },
+      {
+         $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers",
+         },
+      },
+      {
+         $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribedTo",
+         },
+      },
+      {
+         $addFields: {
+            subscribersCount: {
+               $size: "$subscribers",
+            },
+            subscribedToCount: {
+               $size: "$subscribedTo",
+            },
+            isSubscribed: {
+               $cond: {
+                  if: {
+                     $in: [req.user?._id, "$subscribers.subscriber"],
+                  },
+                  then: true,
+                  else: false,
+               },
+            },
+         },
+      },
+      {
+         $project: {
+            fullName: 1,
+            email: 1,
+            username: 1,
+            avatar: 1,
+            coverImage: 1,
+            subscribersCount: 1,
+            subscribedToCount: 1,
+            isSubscribed: 1,
+         },
+      },
+   ]);
+
+   if (!channel?.length) {
+      throw new APIError(404, "Channel/User doesn't exist!!");
+   }
+
+   return res
+      .status(200)
+      .json(
+         new APIResponse(200, channel[0], "Channel retrieve successfully!!")
+      );
+});
+
 export {
    registerUser,
    loginUser,
@@ -375,4 +446,5 @@ export {
    updateAccountDetails,
    updateUserAvatar,
    updateUserCoverImage,
+   getUserChannelProfile,
 };
