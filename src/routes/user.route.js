468e7c3c43ac319b458545c66fba9aae60798d8f
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-07-08 11:38:28 +0600

    add missing routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,13 @@ import {
    loginUser,
    logoutUser,
    refreshAccessToken,
+   changeCurrentPassword,
+   getCurrentUser,
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage,
+   getUserChannelProfile,
+   getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,5 +30,17 @@ router.route("/refresh-token").post(refreshAccessToken);
 
 // Secure route
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-info").patch(verifyJWT, updateAccountDetails);
+router
+   .route("avatar")
+   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+   .route("cover-image")
+   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
